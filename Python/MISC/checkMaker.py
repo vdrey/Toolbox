3891b2d68874bfaa2b844a2e3a21f91630b30164
vdrey <daniel7reynolds@gmail.com>
2014-07-10 09:37:03 -0400

    Edits

--- Python/MISC/checkMaker.py
+++ Python/MISC/checkMaker.py
@@ -3,14 +3,19 @@ import random
 
 print('Enter dollar ammount of check values needed.')
 amt = input()
+print()
+print()
 
 checkVals = []
 
 while sum(checkVals) < amt:
 
     nextVal = random.uniform(0, amt)
+    nextVal = round(nextVal, 2)
 
     if nextVal + sum(checkVals) > amt:
 
     else:
         checkVals.append(nextVal)
+
+print(check
