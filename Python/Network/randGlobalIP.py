5b1c53267fe01fc61b048f02295f231eb4f13b19
Daniel Reynolds <vdrey@users.noreply.github.com>
2015-02-01 16:00:58 -0500

    Edit randGlobalIP.py

--- Python/Network/randGlobalIP.py
+++ Python/Network/randGlobalIP.py
@@ -2,14 +2,18 @@
 # It generates public or private (even if the private ips aren't available)
 
 # IP address in form A.B.C.D
+import random
+
+def randOct(x,y):
+    return str(random.randint(x,y)
+
 
 def globalIP():
     A = str(192)
     B = str(168)
     C = str(0)
 
-    import random
-
+    
     D = random.randint(1,256)
     D = str(D)
 
