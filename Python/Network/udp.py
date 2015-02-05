47de7f67222ff380374bbceb9f602daa3842c0d4
Daniel Reynolds <vdrey@users.noreply.github.com>
2015-02-04 21:44:54 -0500

    Tried udp.py with broadcast ip It says permission denied );

--- Python/Network/udp.py
+++ Python/Network/udp.py
@@ -1,14 +1,14 @@
 import socket
 
 def sendPkt(): 
-    UDP_IP= "192.168.0.15"
+    UDP_IP= "192.168.0.254"
     UDP_PORT= 5005
     MESSAGE= "Hello world! This a test!"
  
  
-    print("UDP target IP:", UDP_IP)
-    print("UDP target port:", UDP_PORT)
-    print("message:", MESSAGE)
+    #print("UDP target IP:", UDP_IP)
+    #print("UDP target port:", UDP_PORT)
+    #print("message:", MESSAGE)
  
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
     sock.sendto(bytes(MESSAGE, "utf-8"), (UDP_IP, UDP_PORT))
