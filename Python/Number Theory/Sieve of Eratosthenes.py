65d57d959383b82844121f8ba29073fea666b7a8
Daniel Reynolds <vdrey@users.noreply.github.com>
2015-02-21 15:34:18 -0500

    Removed non-working files

--- Python/Number Theory/Sieve of Eratosthenes.py
+++
@@ -1,9 +0,0 @@
-def primes(n):
-    if n<=2:
-        return []
-    sieve=[True]*(n+1)
-    for x in range(3,int(n**0.5)+1,2):
-        for y in range(3,(n//x)+1,2):
-            sieve[(x*y)]=False
-
-    return [2]+[i for i in range(3,n,2) if sieve[i]]
