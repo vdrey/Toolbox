a2b0aa5d4ffd628a50878b05acbfdc8cc0915e8e
Daniel Reynolds <vdrey@users.noreply.github.com>
2015-03-21 13:29:54 -0400

    Edited frankenstein.py

--- Python/MISC/frankenstein.py
+++ Python/MISC/frankenstein.py
@@ -11,12 +11,14 @@ wordList = {}
 for line in lineList:
     words = line.split(' ')
     for word in words:
+        word = word.upper()
         if word not in wordList:
             wordList[word] = 1
         else:
             wordList[word] += 1
 
 sorted_wordList = sorted(wordList.items(), key=operator.itemgetter(1))
+sorted_wordList.reverse()
 
-for i in range(len(sorted_wordList) - 100, len(sorted_wordList)):
+for i in range(10):
     print(sorted_wordList[i])
