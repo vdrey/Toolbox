6d2a78df3e47b1b105629220d07bb9d742eeaeec
Daniel Reynolds <vdrey@users.noreply.github.com>
2015-04-12 12:17:34 -0400

    Tweak sms.py

--- Python/sms.py
+++ Python/sms.py
@@ -1,13 +1,18 @@
 # Send those sms
-import requests 
-url = 'http://textbelt.com/text' 
+import requests
+
+url = 'http://textbelt.com/text'
+
+
 def sendSMS(num, content):
     number = None
-    message= None
+    message = None
 
     stringNum = str(num)
     payload = {}
-    payload[number]=stringNum
-    payload[message]=content
+    payload[number] = num
+    payload[message] = content
     m = requests.post(url, data=payload)
-sendSMS(7042925045, 'Hello Alex')
+
+
+sendSMS(7042925045, 'Hello Alex. This was sent by the python program.')
