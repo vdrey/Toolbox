3ab1a1e4cf8e62f3e4c8512749643625da20c30d
Daniel Reynolds <vdrey@users.noreply.github.com>
2015-04-25 19:00:56 -0400

    edits

--- Python/sms.py
+++ Python/sms.py
@@ -1,8 +1,6 @@
 # Send those sms
 import requests
 
-
-
 def sendSMS(num, content):
     headers = {'User-Agent':'LOL'}
     url = 'http://textbelt.com/text'
@@ -12,15 +10,4 @@ def sendSMS(num, content):
     print()
     print(payload)
 
-sendSMS(7045552222, 'I wanted to test if the connection to the server is encryptable. Please let me know if you get this. It will be the last one today')
-
-"""
-def send_text(numstring, message):
-    message = {"number": numstring, "message": message}
-    r = requests.post("http://textbelt.com/text", data=message)
-    print(r.status_code)
-    print()
-    print(r.text)
-    return r.status_code, r.text
-"""
-
+sendSMS(7045552222, 'This is my message')
